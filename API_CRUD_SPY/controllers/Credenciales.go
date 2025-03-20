6ab80fae8f493d6702c0ed34665bb69b1c3ef903
Zseiru15 <co4786696@gmail.com>
2025-03-20 16:27:01 -0500

    Arreglo de controladores

--- API_CRUD_SPY/controllers/Credenciales.go
+++ API_CRUD_SPY/controllers/Credenciales.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"errors"
-	"github.com/Zseiru15/System-Parking-Yopal-BackEnd-CRUD/API_CRUD_SPY/models"
+	"github.com/Zseiru15/System-Parking-Yopal-BackEnd/API_CRUD_SPY/models"
 	"strconv"
 	"strings"
 
@@ -34,14 +34,34 @@ func (c *CredencialesController) URLMapping() {
 func (c *CredencialesController) Post() {
 	var v models.Credenciales
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
+		// Asignar Estado a true si no se especifica
+		if !v.Estado {
+			v.Estado = true
+		}
 		if _, err := models.AddCredenciales(&v); err == nil {
 			c.Ctx.Output.SetStatus(201)
-			c.Data["json"] = v
+			c.Data["json"] = map[string]interface{}{
+				"succes": true,
+				"status": 201,
+				"message": "creacion generada correctamente",
+				"data":    v}
 		} else {
 			c.Data["json"] = err.Error()
+			c.Ctx.Output.SetStatus(500)
+			c.Data["json"] = map[string]interface{}{
+				"success": false,
+				"status":  500,
+				"message": err.Error(),
+			}
 		}
 	} else {
 		c.Data["json"] = err.Error()
+		c.Ctx.Output.SetStatus(400)
+		c.Data["json"] = map[string]interface{}{
+			"success": false,
+			"status":  400,
+			"message": err.Error(),
+		}
 	}
 	c.ServeJSON()
 }
@@ -60,7 +80,11 @@ func (c *CredencialesController) GetOne() {
 	if err != nil {
 		c.Data["json"] = err.Error()
 	} else {
-		c.Data["json"] = v
+		c.Data["json"] = map[string]interface{}{
+			"succes": true,
+			"status": 200,
+			"message": "consulta realizada correctamente",
+			"data":    v}
 	}
 	c.ServeJSON()
 }
@@ -123,7 +147,11 @@ func (c *CredencialesController) GetAll() {
 	if err != nil {
 		c.Data["json"] = err.Error()
 	} else {
-		c.Data["json"] = l
+		c.Data["json"] = map[string]interface{}{
+			"succes": true,
+			"status": 200,
+			"message": "consulta realizada correctamente",
+			"data":    l}
 	}
 	c.ServeJSON()
 }
@@ -143,6 +171,11 @@ func (c *CredencialesController) Put() {
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
 		if err := models.UpdateCredencialesById(&v); err == nil {
 			c.Data["json"] = "OK"
+			c.Data["json"] = map[string]interface{}{
+				"succes": true,
+				"status": 200,
+				"message": "actualizacion realizada correctamente",
+				"data":    v}
 		} else {
 			c.Data["json"] = err.Error()
 		}
@@ -164,6 +197,11 @@ func (c *CredencialesController) Delete() {
 	id, _ := strconv.Atoi(idStr)
 	if err := models.DeleteCredenciales(id); err == nil {
 		c.Data["json"] = "OK"
+		c.Data["json"] = map[string]interface{}{
+			"succes": true,
+			"status": 200,
+			"message": "se elimino correctamente",
+			"dato eliminado con id": id}
 	} else {
 		c.Data["json"] = err.Error()
 	}
