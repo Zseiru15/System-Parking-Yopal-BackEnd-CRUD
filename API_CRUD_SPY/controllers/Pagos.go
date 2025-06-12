e8446383434ddbbc8891f0d7758233944ac73060
Zseiru15 <co4786696@gmail.com>
2025-06-12 17:23:53 -0500

    modificacion de pagos y usuarios, y actualizacion de la BD

--- API_CRUD_SPY/controllers/Pagos.go
+++ API_CRUD_SPY/controllers/Pagos.go
@@ -53,13 +53,12 @@ func (c *PagosController) Post() {
 			c.Ctx.Output.SetStatus(500)
 		}
 	} else {
-		c.Ctx.Output.SetStatus(400)
 		c.Data["json"] = map[string]interface{}{
 			"success": false,
 			"status":  400,
-			"message": "Error al decodificar el cuerpo de la solicitud",
-			"error":   err.Error(),
+			"message": err.Error(),
 		}
+		c.Ctx.Output.SetStatus(400)
 	}
 	c.ServeJSON()
 }
--- API_CRUD_SPY/models/Pagos.go
+++ API_CRUD_SPY/models/Pagos.go
@@ -11,15 +11,18 @@ import (
 )
 
 type Pagos struct {
-	Id                   int               `orm:"column(Id_Pagos);pk"`
+	Id                   int               `orm:"column(Id_Pagos);pk;auto"`
 	IdUsuariosFk         *Usuarios         `orm:"column(Id_Usuarios_fk);rel(fk)"`
-	IdEstacionamientosFk *Estacionamientos `orm:"column(Id_Estacionamientos_fk);rel(fk)"`
+	IdEstacionamientosFk *Estacionamientos `orm:"column(Id_Estacionamientos_fk);rel(fk);null"`
 	PayPalOrderID        string            `orm:"column(PayPal_Order_ID)"`
+	PayPalCaptureID      string            `orm:"column(PayPal_Capture_ID);null"`         // nuevo
+	PayerID              string            `orm:"column(Payer_ID);null"`                 // nuevo
+	PayerName            string            `orm:"column(Payer_Name);null"`               // nuevo
+	PayerEmail           string            `orm:"column(Payer_Email);null"`
 	Amount               float64           `orm:"column(Amount);null"`
 	Currency             string            `orm:"column(Currency);null"`
-	Status               bool              `orm:"column(Status);null"`
-	PayerEmail           string            `orm:"column(Payer_Email);null"`
-	ReceiverEmail        string            `orm:"column(Receiver_Email);null"`
+	Status               string            `orm:"column(Status);null"`                   // cambiado de bool a string
+	PayPalCreateTime     time.Time         `orm:"column(PayPal_Create_Time);null"`       // nuevo
 	CreatedAt            time.Time         `orm:"column(Created_At);type(timestamp with time zone);null;auto_now_add"`
 	Tipo                 string            `orm:"column(Tipo);null"`
 }
